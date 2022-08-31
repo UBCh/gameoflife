d0e7b46fa9927137e771c54c300286449592b74d
UBCh <89996798+UBCh@users.noreply.github.com>
2022-08-31 16:14:21 +0300

    Update GameOfLife.java

--- src/main/java/org/example/GameOfLife.java
+++ src/main/java/org/example/GameOfLife.java
@@ -9,132 +9,93 @@ import java.io.IOException;
 
 public class GameOfLife {
 
-  private  int y;
-  private  int x;
-  private  int numberOfLives;
-  private  char[][] fieldOfLife;
-  private String path="src/test/resources/";
+  private  int verticalSize;
+  private  int horizontalSize;
+  private  int numberOfCycles;
+  private  char[][] originalPlayingField;
 
-
-  public void game(String fileNameInput, String fileNameOutput) throws IOException {
+   public void game(String fileNameInput, String fileNameOutput) {
    inputFile(fileNameInput);
-   lifeCycle();
-    outputFile(fileNameOutput);
-  }
+   lifeCycle( );
+   outputFile(fileNameOutput);
+ }
 
-  public  void setY(int vertically) {
-    y = vertically;
-  }
-
-  public  void setX(int horizontally) {
-    x = horizontally;
-  }
-
-  public  void setNumberOfLives(int numberOL) {
-    numberOfLives = numberOL;
-  }
-
-
-
-  public  void inputFile(String fileNameInput) throws IOException {
-    FileReader fr = new FileReader(path+fileNameInput);
+  public  void inputFile(String fileNameInput)  {
+    String path="src/test/resources/";
+  try {FileReader fr = new FileReader(path+fileNameInput);
     BufferedReader reader = new BufferedReader(fr);
     String line = reader.readLine( );
     String[] taskСonditions = line.split(",");
-    int y = Integer.parseInt(taskСonditions[0]);
-    setY(y);
-    int x = Integer.parseInt(taskСonditions[1]);
-    setX(x);
-    setNumberOfLives(Integer.parseInt(taskСonditions[2]));
-    char[][] input = new char[y][x];
+    verticalSize=Integer.parseInt(taskСonditions[0]);
+    horizontalSize= Integer.parseInt(taskСonditions[1]);
+    numberOfCycles=Integer.parseInt(taskСonditions[2]);
+    char[][] input = new char[verticalSize][horizontalSize];
     char[] tmp ;
-    for (int i = 0; i < y; i++) {
+    for (int i = 0; i < verticalSize; i++) {
       line = reader.readLine( );
       if (line==null){ break;}
       line = line.replace(" ", "");
       tmp = line.toCharArray( );
-      for (int j = 0; j < x; j++) {
-        input[i][j] = tmp[j];
-      }
-
-    }
-    setFieldOfLife(input);
-
-  }
-
-  private   void setFieldOfLife(char[][] chars) {
-   fieldOfLife = chars;
+      for (int j = 0; j < horizontalSize; j++) {input[i][j] = tmp[j];}}
+    originalPlayingField =input;}
+  catch (IOException e) {
+    e.printStackTrace( ); }
   }
 
   public  void lifeCycle() {
     char[][] newLife;
-    for (int life = 0; life < numberOfLives; life++) {
-      newLife = new char[y][x];
-      for (int i = 0; i < y; i++) {
-
-        for (int j = 0; j < x; j++) {
-
-          newLife[i][j] = weСheckЕheМiability(i, j);
-
-        }
-
-      }
-      fieldOfLife = newLife;
-
-    }
-
+    for (int life = 0; life < numberOfCycles; life++) {
+      newLife = new char[verticalSize][horizontalSize];
+      for (int i = 0; i < verticalSize; i++) {
+        for (int j = 0; j < horizontalSize; j++) {
+          newLife[i][j] = viabilityCheck(i, j);}}
+      originalPlayingField = newLife; }
   }
 
-  private  char weСheckЕheМiability(int v, int h) {
-    int lifeСounter = 0;
-    char celuli = fieldOfLife[v][h];
+  private char viabilityCheck(int verticallyCoordinate, int horizontalCoordinate) {
+    int counter = 0;
+    char celuli = originalPlayingField[verticallyCoordinate][horizontalCoordinate];
     char tmp;
     int verticali;
     int horizontali;
-
     for (int i = -1; i < 2; i++) {
       for (int j = -1; j < 2; j++) {
-        verticali = (v + i + y) % y;
-        horizontali = (h + j + x) % x;
-        tmp = fieldOfLife[verticali][horizontali];
-        if (tmp == 'X') {
-          lifeСounter++;
-        }
-      }
+        verticali = (verticallyCoordinate + i + verticalSize) % verticalSize;
+        horizontali = (horizontalCoordinate + j + horizontalSize) % horizontalSize;
+        tmp = originalPlayingField[verticali][horizontali];
+        if (tmp == 'X') {counter++;}}
     }
-    if (celuli=='X') {
-      lifeСounter --;
-      return getCellByState(shouldAliveCellLive(lifeСounter));
-    }
-    else {return getCellByState(shouldDeadCellLive(lifeСounter));}
-
+    if (celuli == 'X') { counter--; return getCellByState(shouldAliveCellLive(counter));}
+    else { return getCellByState(shouldDeadCellLive(counter));}
   }
 
-  private static boolean shouldAliveCellLive(int lifCounter) {
-    return lifCounter > 1 && lifCounter < 4;
+  private boolean shouldAliveCellLive(int counter) {
+    return counter > 1 && counter < 4;
   }
 
-  private static boolean shouldDeadCellLive(int lifCounter) {
-    return lifCounter == 3;
+  private boolean shouldDeadCellLive(int counter) {
+    return counter == 3;
   }
 
-  private static char getCellByState(boolean state) {
+  private char getCellByState(boolean state) {
     return state ? 'X' : 'O';
   }
 
-  public  void outputFile(String fileNameOutput) throws IOException {
+  public void outputFile(String fileNameOutput) {
+    String path = "src/test/resources/";
     StringBuilder stringBuilder = new StringBuilder( );
-    char[][] tmp = fieldOfLife;
-    for (int G = 0; G < y; G++) {
-      for (int E = 0; E < x; E++) {
-        stringBuilder.append(tmp[G][E]).append(" ");
+    char[][] tmp = originalPlayingField;
+    for (int i = 0; i < verticalSize; i++) {
+      for (int j = 0; j < horizontalSize; j++) {
+        stringBuilder.append(tmp[i][j]).append(" ");
       }
       stringBuilder.deleteCharAt(stringBuilder.length( ) - 1);
       stringBuilder.append("\n");
-      BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path+fileNameOutput)));
-      out.append(stringBuilder);
-      out.close( );
-    }
-
+      try {
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path + fileNameOutput)));
+        out.append(stringBuilder);
+        out.close( );
+      } catch (IOException e) {
+        e.printStackTrace( );}}
   }
 }
